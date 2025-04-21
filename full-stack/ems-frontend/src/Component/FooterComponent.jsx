71171990f41cc3af3d87a0137bde2f6e7160a009
varshi-git <varshika27015@gmail.com>
2025-04-21 11:30:57 +0530

    file uploads

---
+++ full-stack/ems-frontend/src/Component/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reservered by code</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
