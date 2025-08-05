42082cad1d3050a07090482b24e19ad27458d2ed
varshi-git <varshika27015@gmail.com>
2025-08-06 00:03:22 +0530

    varshika tha mass uh 

--- ems-backend/ems-backend/src/main/java/code/ems_backend/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package code.ems_backend.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
--- ems-backend/ems-backend/src/main/java/code/ems_backend/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package code.ems_backend.repository;
-
-import code.ems_backend.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
